26c216fc5b417a6c20e5ba5345d673aaf8f22cbb
Betelhem Yemane <betelhemyemanezoom@gmail.com>
2025-04-01 15:30:25 -0700

    fix error

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import TelegramBot from 'node-telegram-bot-api';
-import { start, menu } from './handlers/commands.js';
+import { start } from './handlers/commands.js';
 import { exampleCallback } from './handlers/callbacks.js';
 
 // Initialize bot
@@ -8,7 +8,6 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
 // Commands
 bot.onText(/\/start/, (msg) => start(bot, msg));
-bot.onText(/\/menu/, (msg) => menu(bot, msg));
 
 // Callbacks
 bot.on('callback_query', (query) => exampleCallback(bot, query));
